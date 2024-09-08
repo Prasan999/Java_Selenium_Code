ba2d288cccdf7382a2781690a4b717dee2951307
Prasanna <prasannakumarpc@outlook.com>
2024-09-08 13:31:24 +0530

    new branch-develop

---
+++ src/test/java/com/git/Appium.java
@@ -0,0 +1,16 @@
+package com.git;
+
+import org.testng.annotations.Test;
+
+public class Appium {
+	
+	
+	@Test
+	public void appium1_develop_branch() {
+		
+		System.out.println("pushing appium1_develop_branch method new branch");
+		
+	}
+
+
+}
