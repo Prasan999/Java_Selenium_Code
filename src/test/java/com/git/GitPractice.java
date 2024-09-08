6b659908d28a88357baf64b3bc4b22615874ab5f
Prasanna <prasannakumarpc@outlook.com>
2024-09-08 13:21:49 +0530

    m2 method from GIT Original DIR

--- src/test/java/com/git/GitPractice.java
+++ src/test/java/com/git/GitPractice.java
@@ -9,6 +9,14 @@ public class GitPractice {
 	public void m1() {
 		
 		System.out.println("m1 method");
+		
+	}
+	
+	@Test
+	public void m2() {
+		
+		System.out.println("pushing m2 method from GIT_Repository Original directory");
+		
 	}
 
 }
